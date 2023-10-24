e311a18c9fff614b8a45ba0edbd91a4132d5ad43
ulysse.couliou <ulysse.couliou.etu@univ-lille.fr>
2023-10-24 14:12:03 +0200

    modif monster

--- src/Monster.java
+++ src/Monster.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 
 public class Monster extends Player {
 
     protected Maze maze;
+    ArrayList<Coordinate> deplacementPossible = new ArrayList<Coordinate>();
 
     public Monster(Coordinate c, Maze m){
         this.coordinate = c;
@@ -12,8 +14,15 @@ public class Monster extends Player {
         return maze.maze[c.row][c.col].getState() != CellInfo.WALL;
     }
 
+    public void deplacement(ArrayList<Coordinate> deplacementPossible){
+        int random = (int) (Math.random() * deplacementPossible.size());
+        move(deplacementPossible.get(random));
+    }
+
     public void move(Coordinate c){
         this.coordinate = c;
         notifyObservers();
     }
+
+    
 }
