f6e631f4a0fa814e034f85268ed58f693d83613d
Kellian <kellian.mirey.etu@univ-lille.fr>
2024-01-11 21:56:35 +0100

    fix error from merging

--- src/game/view/IHM.java
+++ src/game/view/IHM.java
@@ -102,7 +102,7 @@ public class IHM extends Application {
             pathExist = maze.checkPathExists();
         }
         
-        hView = new HunterView(this, stage,  this.maze, this.iaHunter);
+        hView = new HunterView(this, stage,  this.maze, this.iaHunter, true);
         mView = new MonsterView(this, stage, this.maze, this.iaMonster);
         mView.setInteractions(true);
     }
